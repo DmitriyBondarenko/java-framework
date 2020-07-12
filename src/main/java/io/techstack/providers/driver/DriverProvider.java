4dd85d29cfa9c03a7455d71f6c149f0b3956f45a
dbondarenko <dmytro.bondarenko@tech-stack.io>
2020-07-12 13:51:45 +0300

    Removed BrowserFactory.java

--- src/main/java/io/techstack/providers/driver/DriverProvider.java
+++ src/main/java/io/techstack/providers/driver/DriverProvider.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.techstack.utils.PropertyReader;
-import io.techstack.utils.BrowserFactory;
 
 /**
  * This class provides methods to interact with WedDriver instance
